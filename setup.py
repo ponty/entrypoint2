5a9cadf2ba58e0563e4b1ea051604a176e700648
ponty <ponty@home>
2020-05-18 18:16:48 +0200

    versioned home link

--- setup.py
+++ setup.py
@@ -3,21 +3,23 @@ import os
 from setuptools import setup
 
 NAME = "entrypoint2"
-URL = "https://github.com/ponty/entrypoint2"
-DESCRIPTION = "easy to use command-line interface for python modules"
-LONG_DESCRIPTION = """easy to use command-line interface for Python modules
-
-home: https://github.com/ponty/entrypoint2"""
-
-PACKAGES = [
-    NAME,
-    NAME + ".examples",
-]
 
 # get __version__
 __version__ = None
 exec(open(os.path.join(NAME, "about.py")).read())
 VERSION = __version__
+
+URL = "https://github.com/ponty/entrypoint2"
+DESCRIPTION = "easy to use command-line interface for python modules"
+LONG_DESCRIPTION = """easy to use command-line interface for Python modules
+
+home: https://github.com/ponty/entrypoint2/tree/"""
+LONG_DESCRIPTION += VERSION
+
+PACKAGES = [
+    NAME,
+    NAME + ".examples",
+]
 
 # extra = {}
 # if sys.version_info >= (3,):
