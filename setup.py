a29f6272c0591394ca334749201134ef011b0301
ponty <ponty@home>
2020-12-30 14:11:53 +0100

    remove py27

--- setup.py
+++ setup.py
@@ -21,10 +21,6 @@ PACKAGES = [
     NAME + ".examples",
 ]
 
-# extra = {}
-# if sys.version_info >= (3,):
-#     extra['use_2to3'] = True
-
 classifiers = [
     # Get more strings from
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -32,9 +28,8 @@ classifiers = [
     "Natural Language :: English",
     "Operating System :: OS Independent",
     "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
     "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
